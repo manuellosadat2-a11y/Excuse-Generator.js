a75d85b6e5cf762d11c69ae793751bc61ed75994
manuellosadat2-a11y <manuellosadat2@gmail.com>
2025-09-23 14:44:36 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let who = ["The dog","My grandma","The mailman","My bird"]
+let action = ["ate","peed","crushed","broke"]
+let what = ["my homework","my phone","the car"]
+let when = ["before the class", "when I was sleeping", "while I was exercising", "during my lunch", "while I was praying"]
+
+function generateExcuse (){
+    let randomWho = who[Math.floor(Math.random() * who.length)];
+    let randomAction = action[Math.floor(Math.random() * action.length)];
+    let randomWhat = what[Math.floor(Math.random() * what.length)];
+    let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+}
+
+window.onload = () =>{
+    document.getElementById("excuse").innerHTML = generateExcuse();
+};
+/*******buttom********/ 
+document.getElementById("newExcuse").addEventListener("click", () => {
+document.getElementById("excuse").innerHTML = generateExcuse();});
